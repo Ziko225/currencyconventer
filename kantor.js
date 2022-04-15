058cc5980b8e6459ef78e445b711e2f05a614a78
Nazar Bychyk <nazar1bychyk@gmail.com>
2022-04-15 13:44:29 +0200

    Change JS

--- kantor.js
+++ kantor.js
@@ -1,13 +1,15 @@
-let inputA = document.querySelector(".js-inputA");
-let resultElement = document.querySelector(".result");
-let formElement = document.querySelector(".article__form");
-let type1 = document.querySelector(".js-type1");
-let type2 = document.querySelector(".js-type2");
-
-formElement.addEventListener("input", () => {
-    let A = inputA.value;
-    let B = type1.value;
-    let C = type2.value;
-    result = (A * B) / C;
-    resultElement.value = result.toFixed(3)
-}); 
+{
+    let formElement = document.querySelector(".article__form");
+    
+    formElement.addEventListener("input", () => {
+        let inputA = document.querySelector(".js-inputA");
+        let resultElement = document.querySelector(".result");
+        let type1 = document.querySelector(".js-type1");
+        let type2 = document.querySelector(".js-type2");
+        let A = inputA.value;
+        let B = type1.value;
+        let C = type2.value;
+        result = (A * B) / C;
+        resultElement.value = result.toFixed(3)
+    }); 
+}
