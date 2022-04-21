8f96f3f1cadd1b621bd5b12b32b065d803efe351
Nazar Bychyk <nazar1bychyk@gmail.com>
2022-04-21 14:47:25 +0200

    Version 1.0

--- kantor.js
+++ kantor.js
@@ -1,15 +1,18 @@
 {
     const formElement = document.querySelector(".article__form");
-    
+    const inputA = document.querySelector(".js-input");
+    const resultElement = document.querySelector(".js-result");
+    const type1 = document.querySelector(".js-selectFrom");
+    const type2 = document.querySelector(".js-selectTo");
+
     formElement.addEventListener("input", () => {
-        const inputA = document.querySelector(".js-inputA");
-        const resultElement = document.querySelector(".result");
-        const type1 = document.querySelector(".js-type1");
-        const type2 = document.querySelector(".js-type2");
         const A = inputA.value;
         const B = type1.value;
         const C = type2.value;
         result = (A * B) / C;
-        resultElement.value = result.toFixed(3)
-    }); 
+        resultElement.value = result.toFixed(2)
+        if (result < 0) {
+            resultElement.value = ""
+        }
+    });
 }
