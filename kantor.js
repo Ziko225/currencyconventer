63642a26c23c9ac860a03f30e4167c44d18b692b
Nazar Bychyk <nazar1bychyk@gmail.com>
2022-04-21 18:34:59 +0200

    Version 1.1

--- kantor.js
+++ kantor.js
@@ -1,18 +1,30 @@
-{
-    const formElement = document.querySelector(".article__form");
-    const inputA = document.querySelector(".js-input");
-    const resultElement = document.querySelector(".js-result");
-    const type1 = document.querySelector(".js-selectFrom");
-    const type2 = document.querySelector(".js-selectTo");
+{  // _________________________________Calculator_________________________________
 
-    formElement.addEventListener("input", () => {
-        const A = inputA.value;
-        const B = type1.value;
-        const C = type2.value;
+    const onFormSumbit = () => {
+        const input = document.querySelector(".js-input");
+        const selectFrom = document.querySelector(".js-selectFrom");
+        const selectTo = document.querySelector(".js-selectTo");
+        CalcResult(input, selectFrom, selectTo)
+    };
+
+    const CalcResult = (input, selectFrom, selectTo, result,) => {
+        const resultElement = document.querySelector(".js-result");
+
+        const A = input.value;
+        const B = selectFrom.value;
+        const C = selectTo.value;
         result = (A * B) / C;
         resultElement.value = result.toFixed(2)
+
         if (result < 0) {
             resultElement.value = ""
         }
-    });
-}
+    };
+
+    const init = () => {
+        formElement = document.querySelector(".article__form");
+        formElement.addEventListener("input", onFormSumbit);
+    };
+    
+    init()
+} // _________________________________Calculator_________________________________
