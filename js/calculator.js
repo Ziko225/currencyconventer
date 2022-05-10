708b461281f91890f5beec05087abe04d527caf2
Nazar Bychyk <nazar1bychyk@gmail.com>
2022-05-10 17:31:29 +0200

    currency detector added

--- js/calculator.js
+++ js/calculator.js
@@ -4,27 +4,32 @@
         const input = document.querySelector(".js-input");
         const selectFrom = document.querySelector(".js-selectFrom");
         const selectTo = document.querySelector(".js-selectTo");
-        CalcResult(input, selectFrom, selectTo)
+        calcResult(input, selectFrom, selectTo);
     };
 
-    const CalcResult = (input, selectFrom, selectTo, result,) => {
+    const calcResult = (input, selectFrom, selectTo) => {
+        const ammount = input.value;
+        const currencyFrom = selectFrom.value;
+        const currencyTo = selectTo.value;
+
+        let result = (ammount * currencyFrom) / currencyTo;
+
+        corrector(result, currencyFrom, currencyTo);
+    };
+
+    const corrector = (result, currencyFrom, currencyTo) => {
         const resultElement = document.querySelector(".js-result");
 
-        const A = input.value;
-        const B = selectFrom.value;
-        const C = selectTo.value;
-        result = (A * B) / C;
-        resultElement.value = result.toFixed(2)
-
-        if (result < 0) {
-            resultElement.value = ""
-        }
-    };
+        resultElement.value = result.toFixed(2);
+        
+        result < 0 ? resultElement.value = "N/A" : resultElement.value;
+        currencyFrom === currencyTo ? resultElement.value = "same currency detected" : resultElement.value;
+    }
 
     const init = () => {
         formElement = document.querySelector(".article__form");
         formElement.addEventListener("input", onFormSumbit);
     };
-    
+
     init()
 } // _________________________________Calculator_________________________________
